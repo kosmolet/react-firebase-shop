ddc1d5cca456c24ef7eaf16acdd9df6f1d2cdf23
kosmolet <svet1199@gmail.com>
2020-12-16 11:09:08 +0100

    setup server

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')(process.env.SK_STRIPE);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => res.status(200).send('it works'));
+
+exports.api = functions.https.onRequest(app);
+
+// http://localhost:5001/gunshop-7b627/us-central1/api
